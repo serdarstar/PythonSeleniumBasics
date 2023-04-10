2f68bd92597591cacc5e48e7c68181e84f99dba7
Serdar Yildiz <serdaryildiz.sdet@gmail.com>
2023-04-10 10:21:36 +0100

    Revisions

--- SeleniumExamples/1 BrowserTest.py
+++ SeleniumExamples/1 BrowserTest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 # I am not giving the path for chromedriver.exe since I put it under scripts folder
-driver =webdriver.Chrome()
+driver =webdriver.Chrome(executable_path=ChromeDriverManager().install())
 driver.get("https://www.amazon.co.uk")
 
 title= driver.title
